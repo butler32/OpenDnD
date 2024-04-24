44184d4a7ad47efccad74f3b34b0fa724d89faf3
Graph-Donte-Crypto <Dimasik841@gmail.com>
2024-04-24 22:46:44 +0300

    Basic interfaces

---
+++ OpenDnD.Interfaces/IAuthService.cs
@@ -0,0 +1,12 @@
+﻿namespace OpenDnD.Interfaces
+{
+    public class AuthToken
+    {
+
+    }
+    public interface IAuthService
+    {
+        public AuthToken Authenticate(Uri address, string login, string password);
+    }
+
+}
---
+++ OpenDnD.Interfaces/ISessionService.cs
@@ -0,0 +1,27 @@
+﻿namespace OpenDnD.Interfaces
+{
+    public class SessionRequest
+    {
+
+    }
+
+    public class Session
+    {
+
+    }
+    public interface ISessionService : ICRUDService<Session, SessionMapRequest>
+    {
+        public void AddPlayerToSession(AuthToken authToken, Guid sessionId, Guid userId);
+        public void RemovePlayerFromSession(AuthToken authToken, Guid sessionId, Guid userId);
+
+        delegate void CurrentSessionMapChanged(SessionMapEntity sessionMapEntity);
+        event CurrentSessionMapChanged OnCurrentSessionMapChanged;
+
+        delegate void SessionChatMessageChanged(SessionChatMessage sessionChatMessage);
+        event SessionChatMessageChanged OnSessionChatMessageChanged;
+
+        public void ChangeCurrentSessionMap(AuthToken authToken, Guid sessionId, Guid sessionMapId);
+
+    }
+
+}
