934ac401bbe68807bbde818180daa00e6695b71c
Graph-Donte-Crypto <Dimasik841@gmail.com>
2024-04-26 00:23:16 +0300

    impl auth service

--- OpenDnD.DB.Services/AuthService.cs
+++ OpenDnD.DB.Services/AuthService.cs
@@ -1,19 +1,64 @@
-﻿using OpenDnD.Interfaces;
+﻿using Microsoft.Extensions.DependencyInjection;
+using OpenDnD.Interfaces;
 
 namespace OpenDnD.DB.Services
 {
     public class AuthService : IAuthService
     {
-        public AuthService(OpenDnDContext openDnDContext)
+        public AuthService(OpenDnDContext openDnDContext, Secret secret)
         {
             OpenDnDContext = openDnDContext;
+            Secret = secret;
         }
 
-        public OpenDnDContext OpenDnDContext { get; }
+        OpenDnDContext OpenDnDContext { get; }
+        Secret Secret { get; }
+
+        public AuthToken Register(Uri address, string login, string password)
+        {
+            if (OpenDnDContext.Players.Any(x => x.UserName == login))
+                throw new Exception("User already exist");
+            var (hash, salt) = CryptoService.GetHashSaltPair(password);
+
+            var user = new Player
+            {
+                PasswordHash = hash,
+                PasswordSalt = salt,
+                PlayerId = Guid.NewGuid(),
+                UserName = login,
+            };
+
+            OpenDnDContext.Players.Add(user);
+            OpenDnDContext.SaveChanges();
+
+            var token = CryptoService.GetAuthToken(user.PlayerId, Secret.SecretKey);
+
+            return new AuthToken { 
+                PlayerId = user.PlayerId,
+                TokenValue = token,
+            };
+
+        }
 
         public AuthToken Authenticate(Uri address, string login, string password)
         {
-            return new AuthToken { };
+            var player = OpenDnDContext.Players.FirstOrDefault(x => x.UserName == login);
+            if (player is null)
+                throw new Exception("User doesn't exist");
+
+            if (!CryptoService.ValidatePasswordSaltHash(password, player.PasswordSalt, player.PasswordHash))
+                throw new Exception("Invalid password");
+
+            var token = CryptoService.GetAuthToken(player.PlayerId, Secret.SecretKey);
+
+            return new AuthToken
+            {
+                PlayerId = player.PlayerId,
+                TokenValue = token,
+            };
         }
+
+        public bool ValidateAuthToken(AuthToken authToken)
+            => CryptoService.ValidateAuthToken(authToken, Secret.SecretKey);
     }
 }
--- OpenDnD.Interfaces/IAuthService.cs
+++ OpenDnD.Interfaces/IAuthService.cs
@@ -1,12 +1,74 @@
-﻿namespace OpenDnD.Interfaces
+﻿using Microsoft.Extensions.ObjectPool;
+using System.Buffers;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Unicode;
+
+namespace OpenDnD.Interfaces
 {
     public class AuthToken
     {
+        public Guid PlayerId { get; set; }
+        public string TokenValue { get; set; }
+    }
 
+    public class Secret
+    {
+        public string SecretKey { get; set; }
     }
     public interface IAuthService
     {
+        public AuthToken Register(Uri address, string login, string password);
         public AuthToken Authenticate(Uri address, string login, string password);
+        public bool ValidateAuthToken(AuthToken authToken);
+    }
+
+    public class CryptoPoolPolicy : PooledObjectPolicy<SHA512>
+    {
+        public override SHA512 Create() => SHA512.Create();
+        public override bool Return(SHA512 obj) => true;
+    }
+    public static class CryptoService
+    {
+        //HASH(id + secret) = token
+        //HASH(Password + salt) = hash
+        private static ObjectPool<SHA512> CryptoPool = new DefaultObjectPool<SHA512>(new CryptoPoolPolicy(), 100);
+        public static string GetAuthToken(Guid playerId, string secretValue)
+            => GetSHA512(Encoding.UTF8.GetString(playerId.ToByteArray()) + secretValue);
+        public static bool ValidateAuthToken(AuthToken authToken, string secretValue)
+            => GetSHA512(Encoding.UTF8.GetString(authToken.PlayerId.ToByteArray()) + secretValue) == authToken.TokenValue;
+        public static (string hash, string salt) GetHashSaltPair(string password)
+        {
+            var salt = GetRandonString();
+            var hash = GetSHA512(password + salt);
+            return (hash, salt);
+        }
+        public static bool ValidatePasswordSaltHash(string password, string salt, string hash)
+            => GetSHA512(password + salt) == hash;
+        
+
+        public static byte[] GetSHA512(byte[] bytes)
+        {
+            var instane = CryptoPool.Get();
+            var hash = instane.ComputeHash(bytes);
+            CryptoPool.Return(instane);
+            return hash;
+        }
+        public static string GetSHA512(string x)
+        {
+            var bytes = Encoding.UTF8.GetBytes(x);
+            var hash = GetSHA512(bytes);
+            var result = Encoding.UTF8.GetString(hash);
+            return result;
+        }
+        public static string GetRandonString()
+        {
+            var bytes = new byte[2048];
+            Random.Shared.NextBytes(bytes);
+            bytes = GetSHA512(bytes);
+            return Encoding.UTF8.GetString(bytes);
+        }
     }
 
 }
