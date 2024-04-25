fe5b15de1da9b16074d07613bca9b045690d1e78
Владимир Дворецкий <plus88228@gmail.com>
2024-04-26 00:53:16 +0300

    add registration button

--- OpenDnD/Windows/LoginWindow.xaml.cs
+++ OpenDnD/Windows/LoginWindow.xaml.cs
@@ -32,7 +32,7 @@ namespace OpenDnD.Windows
         public IAuthService AuthService { get; }
         public IServiceProvider ServiceProvider { get; }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void ConnectButton_Click(object sender, RoutedEventArgs e)
         {
             var authToken = AuthService.Authenticate(new Uri(Address.Text), UserName.Text, Password.Text);
             var ssw = ServiceProvider.GetRequiredService<SessionSelectionWindow>();
@@ -41,6 +41,19 @@ namespace OpenDnD.Windows
             ssw.SetAuthToken(authToken);
             ssw.Show();
             ssw.Begin();
+        }
+
+        private void RegisterButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var authToken = AuthService.Register(new Uri(Address.Text), UserName.Text, Password.Text);
+                MessageBox.Show("Успешно");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
